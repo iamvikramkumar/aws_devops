2a37b35e567acd3f5190ee674cb26a2429188e90
Vikram Kumar <89016145+iamvikramkumar@users.noreply.github.com>
2024-07-15 15:18:09 +0000

    resourceTracker

--- aws_resourceTracker/aws_resourceTracker.sh
+++ aws_resourceTracker/aws_resourceTracker.sh
@@ -46,3 +46,47 @@ aws iam list-users --query 'Users[*].[UserName,UserId,CreateDate,PasswordLastUse
 echo -e "\n-----------------------------------\n" | tee -a $output_file
 echo "End of report" | tee -a $output_file
 
+
+
+
+
+
+# ===========================================
+
+
+#!/bin/bash
+########
+# Author: Vikram
+# Date: 14-07-24
+#
+# Version: v1
+#
+# This script will report the aws resource usage
+########
+
+
+# set -x
+
+# #AWS S3
+# #AWS EC2
+# #AWS Lambda
+# #AWS IAM User
+
+# #List S3 Buckets
+# echo "Print list of S3 buckets"
+# aws s3 ls > resourceTracker
+
+# #List EC2 instances
+# echo "Print list of EC2 buckets"
+# aws ec2 describe-instances | jq '.Reservations[].Instances[].InstanceId'
+
+# #List Lambda
+# echo "Print list of lambda"
+# aws lambda list-functions >> resourceTracker:q!
+
+# #List IAM Users
+# echo "Print list of IAM Users"
+# aws iam list-users
+
+
+# =========================================
